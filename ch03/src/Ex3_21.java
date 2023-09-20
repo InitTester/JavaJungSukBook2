912e20d445d7a50149811bec9dbfb6b247aa3584
InitTester <Initsave@gmail.com>
2023-09-20 22:32:06 +0900

    commit log

---
+++ ch03/src/Ex3_21.java
@@ -0,0 +1,9 @@
+public class Ex3_21 {
+	// OperatorEx21
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(-10%8);
+		System.out.println(10%-8);
+		System.out.println(-10%-8);		
+	}
+}
---
+++ ch03/src/Ex3_26.java
@@ -0,0 +1,17 @@
+public class Ex3_26 {
+	// OperatorEx26
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 3;
+		int y = 5;
+		
+		System.out.println("x는 " + x + "이고, y는 " + y + "일 떄, ");
+		System.out.println("x | y = " + (x|y)); 
+		System.out.println("x & y = " + (x&y));
+		System.out.println("x ^ y = " + (x^y));
+		
+		System.out.println("true | false = " +(true|false));
+		System.out.println("true & false = " +(true&false));
+		System.out.println("true ^ false = " +(true^false));
+	}
+}
