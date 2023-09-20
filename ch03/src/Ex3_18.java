d4b15919593d10d385df056a6c45609d552b55d9
InitTester <Initsave@gmail.com>
2023-09-20 19:42:48 +0900

    commit log

---
+++ ch03/src/Ex3_18.java
@@ -0,0 +1,10 @@
+public class Ex3_18 {
+	// OperatorEx18
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float pi = 3.141592f;
+		float shortPi = Math.round(pi * 1000) / 1000f;
+		
+		System.out.println(shortPi);
+	}
+}
