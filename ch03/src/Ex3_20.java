a1045426b739978e3e074d02e9d084e16f779586
InitTester <Initsave@gmail.com>
2023-09-20 19:47:11 +0900

    commit log

--- ch03/src/Ex3_20.java
+++ ch03/src/Ex3_20.java
@@ -1,10 +1,11 @@
 public class Ex3_20 {
-	// OperatorEx19
+	// OperatorEx20
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int share = 10/8;
-		int remain = 10 % 8;
-		System.out.println("10을 8로 나누면, ");
-		System.out.println("몫은 "+ share + "이고, 나머지는 "+ remain + "입니다.");
+		// i가 1부터 10이 될 때까지, {}안의 문장을 반복 수행한다.
+		for(int i =1; i<=10;i++) {
+			if(i%3==0)
+				System.out.println(i);
+		}
 	}
 }
