6922328e27dbae1811b42cf273598110afb3c650
InitTester <Initsave@gmail.com>
2023-09-22 15:25:44 +0900

    commit log

--- ch06/src/Ex6_16.java
+++ ch06/src/Ex6_16.java
@@ -1,37 +1,39 @@
 public class Ex6_16 {
-	// MyMath2
+	// MemberCall
+	int iv = 10;
+	static int cv = 20;
+	
+	int iv2 = cv;
+	//static int cv2 = iv; // 에러. 클래스변수는 인스턴스 변수를 사용할 수 없음.
+	static int cv2 = new Ex6_16().iv;  // 이처럼 객체를 생성해야 사용가능.
+	
+	static void staticMethod1() {
+		System.out.println(cv);
+		//System.out.println(iv); // 에러. 클래스메서드에서 인스턴스변수를 사용불가.
+		Ex6_16 e = new Ex6_16();
+		System.out.println(e.iv);	// 객체를 생성한 후에야 인스턴스변수의 참조가능.
+	}
+	
+	void instanceMethod1() {
+		System.out.println(cv);
+		System.out.println(iv);	// 인스턴스메서드에서는 인스턴스 변수를 바로 사용가능.
+	}
+	
+	static void staticMethod2() {
+		staticMethod1();
+//		instanceMethod1();	// 에러. 클래스메서드에서는 인스턴스메서드를 호출할 수 없음.
+		Ex6_16 e = new Ex6_16();
+		e.instanceMethod1(); // 인스턴스를 생성한 후에야 호출할 수 있음.
+	}
+	
+	void instanceMethod2() { // 인스턴스메서드에서는 인스턴스메서드와 클래스메서드
+		staticMethod1(); 	// 모두 인스턴스 생성없이 바로 호출이 가능하다.
+		instanceMethod1();
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//클래스메서드호출
-		System.out.println(MyMath2.add(200L, 100L));
-		System.out.println(MyMath2.subtract(200L, 100L));
-		System.out.println(MyMath2.multiply(200L, 100L));
-		System.out.println(MyMath2.divide(200.0, 100.0));
-		
-		MyMath2 mm = new MyMath2();
-		mm.a = 200L;
-		mm.b = 100L;
-		
-		//인스턴스메서드는 객체 생성 후에만 호출이 가능함.
-		System.out.println(mm.add());
-		System.out.println(mm.subtract());
-		System.out.println(mm.multiply());
-		System.out.println(mm.divide());
+		Ex6_16 e = new Ex6_16();
+		e.instanceMethod2();
 	}
-}
-
-class MyMath2 {
-	long a,b;
-	
-	//인스턴스 변수 a,b만을 이용해서 작업하므로 매개변수가 필요없다.
-	long add() { return a+b; } // a,b는 인스턴스변수
-	long subtract() { return a-b; }
-	long multiply() { return a*b; }
-	double divide() { return a/b; }
-	
-	//인스턴스변수와 관계없이 매개변수만으로 작업이 가능하다.
-	static long add(long a, long b) { return a+b; }	// a,b는 지역변수
-	static long subtract(long a, long b) { return a-b; }
-	static long multiply(long a, long b) { return a*b; }
-	static double divide(double a, double b) { return a/b; }
 }
