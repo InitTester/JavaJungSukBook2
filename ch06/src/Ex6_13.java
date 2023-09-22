a8e3ee7a95641d6dbc16fbecdcfa9b7b3a35fae2
InitTester <Initsave@gmail.com>
2023-09-22 13:13:37 +0900

    commit log

---
+++ ch06/src/Ex6_13.java
@@ -0,0 +1,7 @@
+public class Ex6_13 {
+	// MainTest
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		main(null);		// 자기 자신을 다시 호출한다.
+	}
+}
---
+++ ch06/src/Ex6_8.java
@@ -0,0 +1,20 @@
+public class Ex6_8 {
+	// CallStackTest2
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("main(String[] args)이 시작되었음.");
+		firstMethod();
+		System.out.println("main(String[] args)이 끝났음.");
+	}
+	
+	static void firstMethod() {
+		System.out.println("firstMethod()이 시작되었음.");
+		secondMetehod();
+		System.out.println("firstMethod()이 끝났음.");
+	}
+	
+	static void secondMetehod() {
+		System.out.println("secondMethod()이 시작되었음");
+		System.out.println("secondMethod()이 끝났음");
+	}
+}
