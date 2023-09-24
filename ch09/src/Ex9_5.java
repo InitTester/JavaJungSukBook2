8bfbf5e3930770e355dcbc4cc207e16a42b9d47f
InitTester <Initsave@gmail.com>
2023-09-24 23:31:51 +0900

    commit log

---
+++ ch09/src/Ex9_5.java
@@ -0,0 +1,13 @@
+public class Ex9_5 {
+	//ToStringTest
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = new String("KOREA");
+		java.util.Date today = new java.util.Date();
+		
+		System.out.println(str);
+		System.out.println(str.toString());
+		System.out.println(today);
+		System.out.println(today.toString());
+	}
+}
