5f2459ba90e25c485ff87ff39a3c307d6b47d7af
InitTester <Initsave@gmail.com>
2023-09-28 01:08:12 +0900

    0927End

---
+++ ch11/src/Ex11_64.java
@@ -0,0 +1,14 @@
+// StringTokenizerEx2
+import java.util.StringTokenizer;
+
+public class Ex11_64 {
+	public static void main(String[] args) {
+		String expression = "x=100*(200+300)/2";
+		StringTokenizer st = new StringTokenizer(expression,"+-*/=()",true);
+		//StringTokenizer st = new StringTokenizer(expression,"/=()",true);
+		
+		while(st.hasMoreTokens()) {
+			System.out.println(st.nextToken());
+		}
+	}
+}
---
+++ ch11/src/Ex11_67.java
@@ -0,0 +1,23 @@
+// StringTokenizerEx5
+import java.util.StringTokenizer;
+
+public class Ex11_67 {
+	public static void main(String[] args) {
+		String data = "100,,,200,300";
+		
+		// 빈문자열도 토큰으로 인식, 데이터를 토큰으로 잘라낸 결과를 배열에 담아서 반환하기때문에 StringTokenizer보다 성능이 떨어진다
+		String[] result = data.split(","); 
+		StringTokenizer st = new StringTokenizer(data,",");
+		
+		for(int i=0; i <result.length; i++) 
+			System.out.print(result[i]+"|");
+		
+		System.out.println("개수:" + result.length);
+		
+		int i =0;
+		for(;st.hasMoreTokens();i++)
+			System.out.print(st.nextToken()+"|");
+		
+		System.out.println("개수:"+i);
+	}
+}
